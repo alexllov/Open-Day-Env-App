a7522980d583f5f021f25dc1a944ae7a201e7747
alexllov <alexpugh123@gmail.com>
2025-06-13 16:58:30 +0100

    Rolling temp line added

--- apiCaller.js
+++ apiCaller.js
@@ -8,6 +8,12 @@ async function fetchData() {
     const dataArray = await response.json();
     data = dataArray;
     renderData(data[data.length - 1]);
+    // 1 update every 10s -> 360 updates an hour.
+    if (data.length > 360) {
+      updateCharts(data.slice(data.length - 361, data.length - 1));
+    } else {
+      updateCharts(data);
+    }
   } catch (error) {
     document.getElementById("lastData").innerText = "Error fetching data.";
     console.error("Fetch error:", error);
@@ -43,3 +49,145 @@ setInterval(fetchData, 10000);
 // "Temperature (°C)": "23.1227974355337",
 // "Time": "13/06/2025 14:54:32",
 // "Ultra Fine Particulates (PM1.0 ug/m3)": "2"
+
+// Handler Func to update all charts
+function updateCharts(data) {
+  var times = [];
+  var temps = [];
+  var coarseParts = [];
+  var fineParts = [];
+  var ultraFineParts = [];
+  var light = [];
+  var nh3 = [];
+  var oxidisingGasses = [];
+  var reducingGasses = [];
+  var relativeHumidity = [];
+  data.forEach((e) => {
+    times.push(e["Time"]);
+    temps.push(e["Temperature (°C)"]);
+    coarseParts.push(e["Coarse Particulates (PM10 ug/m3)"]);
+    fineParts.push(e["Fine Particulates (PM2.5 ug/m3)"]);
+    ultraFineParts.push(e["Ultra Fine Particulates (PM1.0 ug/m3)"]);
+    light.push(e["Light intensity (Lux)"]);
+    nh3.push(e["NH3 (Ohms)"]);
+    oxidisingGasses.push(e["Oxidising gases (Ohms)"]);
+    reducingGasses.push(e["Reducing gases (Ohms)"]);
+    relativeHumidity.push(e["Relative Humidity (%)"]);
+  });
+  updateTempChart(times, temps);
+  updateHumidChart(times, relativeHumidity);
+}
+
+function calculateRollingAverage(dataArray, interval) {
+  const numericData = dataArray.map((val) => Number(val));
+  let index = interval - 1;
+  const length = dataArray.length + 1;
+  let results = [];
+
+  while (index < length && index < length - interval) {
+    index = index + 1;
+    const intervalSlice = numericData.slice(index - interval, index);
+    const sum = intervalSlice.reduce((prev, curr) => prev + curr, 0);
+    results.push(sum / interval);
+  }
+  return results;
+}
+
+let tempChart;
+function updateTempChart(times, temps) {
+  const tempctx = document.getElementById("tempChart").getContext("2d");
+
+  if (tempChart) {
+    tempChart.destroy();
+  }
+
+  const rollingAvg = calculateRollingAverage(temps, 20);
+
+  tempChart = new Chart(tempctx, {
+    type: "line",
+    data: {
+      labels: times,
+      datasets: [
+        {
+          label: "Rolling Avg",
+          data: rollingAvg,
+          borderColor: "rgba(54, 162, 235, 1)",
+          backgroundColor: "transparent",
+          fill: false,
+          tension: 0.4,
+          clip: false,
+        },
+        {
+          label: "Temperature (°C)",
+          data: temps,
+          borderColor: "rgba(255, 99, 132, 1)",
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          fill: "start",
+          tension: 0.4,
+        },
+      ],
+    },
+    options: {
+      responsive: false,
+      scales: {
+        x: {
+          title: {
+            display: true,
+            text: "Time",
+          },
+        },
+        y: {
+          title: {
+            display: true,
+            text: "Temperature (°C)",
+          },
+          beginAtZero: false,
+        },
+      },
+    },
+  });
+}
+
+let humidChart;
+function updateHumidChart(times, relativeHumidity) {
+  const humidctx = document.getElementById("humidChart").getContext("2d");
+
+  if (humidChart) {
+    humidChart.destroy();
+  }
+
+  humidChart = new Chart(humidctx, {
+    type: "line",
+    data: {
+      labels: times,
+      datasets: [
+        {
+          label: "Relative Humidity (%)",
+          data: relativeHumidity,
+          borderColor: "rgb(99, 232, 255)",
+          backgroundColor: "rgba(99, 206, 255, 0.2)",
+          fill: true,
+          tension: 0.4,
+        },
+      ],
+    },
+    options: {
+      responsive: false,
+      scales: {
+        x: {
+          title: {
+            display: true,
+            text: "Time",
+          },
+        },
+        y: {
+          title: {
+            display: true,
+            text: "Relative Humidity (%)",
+          },
+          beginAtZero: false,
+        },
+      },
+    },
+  });
+}
